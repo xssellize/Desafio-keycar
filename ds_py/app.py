b7376c5ffe1bad3206cb509d1ec8ca8b11c5badd
xssellize <47544397+xssellize@users.noreply.github.com>
2019-07-17 21:39:24 -0300

    Add files via upload

---
+++ ds_py/app.py
@@ -0,0 +1,11 @@
+# flask_web/app.py
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'teste teste 123'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
---
+++ ds_py/migrations/001_create_table_up.sql
@@ -0,0 +1,2 @@
+
+CREATE TABLE clientes (id serial NOT NULL, nome VARCHAR(250) NOT NULL, idade INTEGER, PRIMARY KEY (id));
